1c947a97a97635e8095a9da7ef62783693bde3d3
Samantha Allen <samanthaamiraallen@gmail.com>
2024-04-04 09:11:38 -0300

    addeded use mutation to handle save book

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,8 +9,10 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -59,10 +61,12 @@ const SearchBooks = () => {
     }
   };
 
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -72,7 +76,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: { input: bookToSave }
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
@@ -146,6 +152,7 @@ const SearchBooks = () => {
             );
           })}
         </Row>
+        {error && <div>Something went wrong...</div>}
       </Container>
     </>
   );
